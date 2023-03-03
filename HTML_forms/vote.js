21ead35007c0030e3f92eef4df78090cf13a9941
Gauri Mahashabde <gauri2interact@gmail.com>
2023-03-03 09:32:49 +0530

    New app added

---
+++ HTML_forms/vote.js
@@ -0,0 +1,9 @@
+
+const btn = document.getElementById("submit").value;
+
+function check(){
+    const age = document.getElementById("age").value;
+    if(age === 18){
+        console.log("eligible to vote");
+    }
+}
