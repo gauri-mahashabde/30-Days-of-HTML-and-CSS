2a8fd259531bc6e26abd2096961c60ed934066fd
Gauri Mahashabde <gauri2interact@gmail.com>
2023-03-13 19:04:08 +0530

    New file added

--- HTML_forms/vote.js
+++ HTML_forms/vote.js
@@ -1,9 +1,10 @@
 
-const btn = document.getElementById("submit").value;
+//const btn = document.getElementById("submit").value;
 
 function check(){
     const age = document.getElementById("age").value;
-    if(age === 18){
+    
+    if(age >= 18){
         console.log("eligible to vote");
     }
 }
