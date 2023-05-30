71842ff438d1915b9a0f7d4b3ea1cc4faf156816
Gauri Mahashabde <gauri2interact@gmail.com>
2023-05-30 17:22:34 +0530

    changes made

--- HTML_forms/Application_form/script.js
+++ HTML_forms/Application_form/script.js
@@ -16,8 +16,7 @@ var server = http.createServer(function (req, res) {
       res.write(data.toString());
       res.end();
     });
-  } 
-  else if (req.url == "/register") {
+  } else if (req.url == "/register") {
     console.log("Register page");
     fs.readFile("register.html", function (err, data) {
       if (err) {
@@ -26,59 +25,53 @@ var server = http.createServer(function (req, res) {
       res.write(data.toString());
       res.end();
     });
-  }
-  else if (req.url == "/create")
-  {
-        const MyURL = new URL(req.url, "http://localhost:8080");
-        var fname = MyURL.searchParams.get("name");
-        var email = MyURL.searchParams.get("email");
-        var password = MyURL.searchParams.get("password");
-    
+  } else if (req.url == "/create") {
+    const MyURL = new URL(req.url, "http://localhost:8080");
+    var fname = MyURL.searchParams.get("name");
+    var email = MyURL.searchParams.get("email");
+    var password = MyURL.searchParams.get("password");
 
-      var query = "INSERT into applicants(fname , email , password ) VALUES(?,?,?)";
-      // "INSERT into admin(UserID,Password)Values(?,?)";
-      var values = [fname, email, password];
-      console.log(query);
-      console.log(values);
+    var query =
+      "INSERT into applicants(fname , email , password ) VALUES(?,?,?)";
+    // "INSERT into admin(UserID,Password)Values(?,?)";
+    var values = [fname, email, password];
+    console.log(query);
+    console.log(values);
 
-      var con = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "Gauri@1029",
-        database: "application_form",
-        port: 3306,
-      });
-    
-      con.connect(function (err) {
-        if (err) {
-          console.log("err in connectivity");
-          return;
-        }
-        console.log("connection successful");
+    var con = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "Gauri@1029",
+      database: "application_form",
+      port: 3306,
     });
-  
-        con.query(query, values, function (error, result) {
-            if (error) {
-              console.log("error in query");
-            } else {
-              res.write("Record created");
-              res.end();
-              console.log(result);
-            }
-        });
-    
-    
-    
-    }
-    else if (req.url == "/register.css") {
+
+    con.connect(function (err) {
+      if (err) {
+        console.log("err in connectivity");
+        return;
+      }
+      console.log("connection successful");
+    });
+
+    con.query(query, values, function (error, result) {
+      if (error) {
+        console.log("error in query");
+      } else {
+        res.write("Record created");
+        res.end();
+        console.log(result);
+      }
+    });
+  } else if (req.url == "/register.css") {
     fs.readFile("home.css", function (err, data) {
       if (err) {
         return console.error(err);
       }
       res.write(data.toString());
       res.end();
-    });}
-  else if (req.url == "/login") {
+    });
+  } else if (req.url == "/login") {
     console.log("Login page");
     fs.readFile("login.html", function (err, data) {
       if (err) {
@@ -115,7 +108,7 @@ var server = http.createServer(function (req, res) {
       res.end();
     });
   } else {
-    console.log("else part")
+    console.log("else part");
     console.log("    " + req.url);
   }
 });
