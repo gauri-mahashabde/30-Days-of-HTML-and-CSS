ccf36301c9731c578741b990e41b97c317d39f98
Gauri Mahashabde <gauri2interact@gmail.com>
2023-06-05 20:07:12 +0530

    script modified

--- HTML_forms/Application_form/script.js
+++ HTML_forms/Application_form/script.js
@@ -251,8 +251,8 @@ else if (req.url == "/login") {
                 else {
                   console.log("query excecute");
                   console.log(result);
-                  console.log(result.length);
-                  if(result.length == 1){
+                  console.log(result.affectedRows);
+                  if(result.affectedRows == 1){
                     fs.readFile("home.html" , function(err ,data){
                       if(err){
                         return console.log(err);
@@ -272,6 +272,81 @@ else if (req.url == "/login") {
       
         });
       }
+
+
+//change password section
+      else if (req.url == "/change") {
+        console.log("change    page");
+        fs.readFile("change.html", function (err, data) {
+          if (err) {
+            return console.error(err);
+          }
+          res.write(data.toString());
+          res.end();
+        });
+      } // end of login
+      //path to login and query to verify the credentials 
+      else if (req.url == "/changeIt") {
+      
+        const MyURL = new URL(req.url, "http://localhost:8080");
+        let body = '';
+          req.on('data' , chunk => {
+            body += chunk.toString();
+          });
+          req.on('end',() => {
+            var post = qs.parse(body);
+            console.log(post);
+    
+            // "INSERT into admin(UserID,Password)Values(?,?)";
+            var query = "UPDATE applicants  SET password=? WHERE email=?";
+            // "INSERT into admin(UserID,Password)Values(?,?)";
+            var values = [ password1,email];
+            console.log(query);
+            console.log(values);
+        
+            var con = mysql.createConnection({
+              host: "localhost",
+              user: "root",
+              password: "Gauri@1029",
+              database: "application_form",
+              port: 3306,
+            });
+    
+            con.connect(function (err) {
+              if (err) {
+                console.log("err in connectivity");
+               return;
+              }
+              else{
+    
+                con.query(query , values , function(error , result){
+                  if(error){
+                    console.log("error in query")
+                  }
+                  else {
+                    console.log("query excecute");
+                    console.log(result);
+                    console.log(result.affectedRows);
+                    if(result.affectedRows == 1){
+                      fs.readFile("home.html" , function(err ,data){
+                        if(err){
+                          return console.log(err);
+                        } 
+                        res.write(data.toString());
+                        res.end();
+                      });
+                    }
+                    else{
+                          res.write("username password is invalid , Try again");
+                          res.end();
+                    } 
+                  }
+                });
+              }
+            });
+        
+          });
+        }
   //css page for register
  else if (req.url == "/register.css") {
   fs.readFile("register.css", function (err, data) {
